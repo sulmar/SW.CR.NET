195ec09bfa2e71f9c76a5725327fe4d7a1bbdf20
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-18 21:23:40 +0200

    Add project files.

---
+++ SW.CR.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SW.CR.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
