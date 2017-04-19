039e8ca0b52b7146c151c4e265d8cf880761e449
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-04-19 11:03:14 +0200

    Załadowanie raportu i wygenerowanie pliku PDF

--- SW.CR.NET/Program.cs
+++ SW.CR.NET/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SW.CR.NET
+namespace SW.CR.NET.ConsoleClient
 {
     class Program
     {
         static void Main(string[] args)
         {
+            LoadReportTest();
+
+        }
+
+        private static void LoadReportTest()
+        {
+            var rpt = new ReportDocument();
+
+            var reportFilename = @"Reports\SimpleReport.rpt";
+
+            rpt.Load(reportFilename);
+
+            if (rpt.IsLoaded)
+            {
+                Console.WriteLine("Report was loaded.");
+
+                rpt.ExportToDisk(CrystalDecisions.Shared.ExportFormatType.PortableDocFormat, "SimpleReport.pdf");
+            }
+
+            rpt.Close();
+
+            rpt.Dispose();
+
+            System.Diagnostics.Process.Start("SimpleReport.pdf");
         }
     }
 }
